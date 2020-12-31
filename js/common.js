9f945718f00a84bc543a46d7431b409b522f71be
KP10535-OLT\장평우 <pwjang@kcp.co.kr>
2020-12-31 15:17:06 +0900

    마무리작업

--- js/common.js
+++ js/common.js
@@ -1,3 +1,17 @@
 $(function(){
 	
+	// 헤더 상단 붙임
+	$(window).on('scroll' , headerFixed)
+
+	function headerFixed(){
+		var headerEl = $("header");
+		var headerH = headerEl.height();
+		var windowT = $(window).scrollTop();
+
+		if(windowT >= headerH){
+			headerEl.addClass('active');
+		} else {
+			headerEl.removeClass('active');
+		}
+	}
 })
